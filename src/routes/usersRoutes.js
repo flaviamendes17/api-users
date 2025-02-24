1a1a7fbd39c92f1ab7968a57030cf8910799e64a
Flavia Mendes <flavinharamendes@gmail.com>
2025-02-24 17:28:20 -0300

    feat: finalização

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -5,5 +5,9 @@ const router = express.Router();
 const usersControllers = require('../controllers/usersControllers');
 
 router.get('/', usersControllers.getAllUsers);
+router.post('/', usersControllers.addUser);
+router.get('/:id', usersControllers.getUserById);
+router.put('/:id', usersControllers.updateUser);
+router.delete('/:id', usersControllers.deleteUser);
 
 module.exports = router;
